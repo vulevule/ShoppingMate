6faef24576170808463206b667963e97b1460fea
Dusan Miljkovic <dusanmiljkovic_95@hotmail.com>
2020-05-11 12:29:48 +0200

    Connected welcome screen and register screen

--- ShoppingMate/app/src/main/java/com/pma/shoppingmate/WelcomeScreenActivity.java
+++ ShoppingMate/app/src/main/java/com/pma/shoppingmate/WelcomeScreenActivity.java
@@ -2,7 +2,9 @@ package com.pma.shoppingmate;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class WelcomeScreenActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class WelcomeScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_screen);
+    }
+
+    public void register(View view) {
+        Intent intent = new Intent(this, RegisterActivity.class);
+        startActivity(intent);
     }
 }
